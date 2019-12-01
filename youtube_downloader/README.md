21e47e6ed023fdcff09bf5d3206b01088c2031ff
Arnav Rupde <34592221+ArnavRupde@users.noreply.github.com>
2019-12-01 10:18:23 +0530

    Add files via upload

---
+++ youtube_downloader/README.md
@@ -0,0 +1,38 @@
+# Youtube Video Downloader
+Using Flask Framework and pytube library for Python
+
+
+<h3>Setting up Modules and Keys</h3>
+<ul>
+<li>Install Flask by typing the following command on terminal - sudo pip install flask</li>
+<li>Install pytube - sudo pip install pytube</li>
+<li>Create the desired frontend and link the backend script.</li>
+</ul>
+<hr />
+ 
+<h3>Running the application</h3>
+<ul>
+<li>Run the python script</li>
+<li>Now open - http://localhost:5000 on your browser</li>
+<li>The application is displayed.</li>
+</ul>
+
+<h3>Steps to use the application.</h3>
+<ol>
+<li>Paste the url of the desired youtube video in the form.</li>
+<li>Hit "Submit" button.</li>
+<li>Now, the list of all the available quality formats are visible to be chosen from.</li>
+<li>Select the desired video quality.</li>
+<li>Enter the path in your pc where video should be saved.</li>
+<li>Hit "Download".</li>
+<li>You will be directed to another page when the video will be downloaded completely.</li>
+
+<br><img src="https://raw.githubusercontent.com/akshayraghav/youtube_downloader/master/homepage.PNG"><br>
+If the URL entered doesn't belong to www.youtube.com, then a pop-up message appears displaying -"Invalid URL".
+<br><img src="https://raw.githubusercontent.com/akshayraghav/youtube_downloader/master/step2.PNG"><br>
+If none of the radio button is selected then a pop-up message appears displaying -"Please select an Option to download".
+If the saving path is not specified than a pop-up meassage appears displaying -"Please enter a valid destination path to save file".
+<br><img src="https://raw.githubusercontent.com/akshayraghav/youtube_downloader/master/step3.PNG"><br>
+The animation appears to emphasize that downloading is in process.
+<br><img src="https://raw.githubusercontent.com/akshayraghav/youtube_downloader/master/final.PNG"><br>
+The video has been downloaded.
---
+++ youtube_downloader/main.py
@@ -0,0 +1,96 @@
+from pytube import YouTube
+from flask import Flask,redirect, url_for,render_template,request
+import requests, random,os
+requests.packages.urllib3.disable_warnings()
+import ssl
+
+app=Flask(__name__)
+
+yt= None
+rng = None
+list = None
+video = None
+
+def fetch(url):
+    global yt
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        # Legacy Python that doesn't verify HTTPS certificates by default
+        pass
+    else:
+        # Handle target environment that doesn't support HTTPS verification
+        ssl._create_default_https_context = _create_unverified_https_context
+
+    try:
+        yt = YouTube(url)
+    except Exception:
+        return -1
+    else:
+        return yt.streams.all()
+
+@app.route('/')
+def renderHomePage():
+    return render_template("index.html")
+
+
+
+@app.route('/work', methods=['POST','GET'])
+def work():
+    global list
+    global rng
+    if request.method == 'POST':
+        url_dw = request.form['url']
+        y='https://www.youtube.com/watch'
+        if y in url_dw:
+            list=fetch(url_dw)
+            if list==-1:
+                return render_template('index.html', eval=1)
+            p=len(list)
+            rng=[]
+            for k in range(p):
+                rng.append(k)
+            return render_template('download.html', packet=zip(list,rng))
+        else:
+            return render_template('index.html',eval=1)
+
+
+@app.route('/success')
+def success():
+    global video
+    render_template('success.html')
+    video.download("F:/Pytube downloads/")
+    return redirect(url_for('renderHomePage'))
+
+
+@app.route('/receive', methods=['POST','GET'])
+def receive():
+    global list
+    global video
+    global rng
+    global yt
+    try:
+        choice = int(request.form['category'])
+    except Exception:
+        return render_template('download.html',packet=zip(list,rng),eval=1)
+    else:
+        video=list[choice]
+        try:
+            home = os.path.expanduser('~')
+            save_at = os.path.join(home, 'Downloads')
+            #save_at=os.path.dirname(os.path.abspath(__file__)) + '/Downloads/'
+            if save_at=='':
+                return render_template('download.html',packet=zip(list,rng),eval=2)
+            video.download(save_at)
+        except Exception:
+            num=str(random.randrange(1,10))
+            nm=video.filename
+            name=nm+num
+            yt.set_filename(name)
+            video.download(save_at)
+            return render_template('success.html',vid=video,path=save_at)
+        else:
+            return render_template('success.html', vid=video,path=save_at)
+
+if __name__== '__main__':
+    app.run(debug='true')
